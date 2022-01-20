357b2f43f9fd1825895efbb2f9c38cc41e90b381
Andrey <3451z@mail.ru>
2022-01-20 13:20:58 +0300

    csv file used

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,24 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
 
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    public void shouldCalculate(String testName, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
-
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
 
-    @org.junit.jupiter.api.Test
+    /* @org.junit.jupiter.api.Test
     void shouldCalculateForRegisteredAndNoAmount() {
         BonusService service = new BonusService();
         long amount = 0;
@@ -105,5 +101,6 @@ public class BonusServiceTest {
         long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
-    }
+    }*/
+
 }
